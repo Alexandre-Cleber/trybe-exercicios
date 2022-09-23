784fcd678c583ec8135414f246b829cab2030045
Alexandre-Cleber <kleber.121@hotmail.com>
2022-09-23 12:03:09 -0300

    segundo exercicio

--- fundamentos/secao-04-intro-a-javascript/exercicio-04.js
+++ fundamentos/secao-04-intro-a-javascript/exercicio-04.js
@@ -1,8 +1,15 @@
-const a = 20;
-const b = 14;
+const a = 25;
+const b = 64;
 
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
-console.log(a % b);
+//console.log(a + b);
+//console.log(a - b);
+//console.log(a * b);
+//console.log(a / b);
+//console.log(a % b);
+
+if (a > b){
+    console.log("'a' é maior que 'b'");
+}
+else {
+console.log ("'b' é maior que 'a'");
+}
