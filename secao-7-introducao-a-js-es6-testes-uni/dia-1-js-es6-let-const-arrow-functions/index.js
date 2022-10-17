ecf8eda6aed1a016dd2dd5240b5845e188a60e71
Alexandre-Cleber <kleber.121@hotmail.com>
2022-10-17 19:54:43 -0300

    exercicios dia 7.1

--- secao-7-introducao-a-js-es6-testes-uni/dia-1-js-es6-let-const-arrow-functions/index.js
+++ secao-7-introducao-a-js-es6-testes-uni/dia-1-js-es6-let-const-arrow-functions/index.js
@@ -6,13 +6,33 @@
 const testingScope = (escopo) => {
     if (escopo === true) {
       let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-      ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+      ifScope = `${ifScope}  ótimo, fui utilizada no escopo !`;
       console.log(ifScope);
     } else {
       const elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
       console.log(elseScope);
     }
-    console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+    console.log(`${ifScope}  o que estou fazendo aqui ? :O`); // Se necessário esta linha pode ser removida.
   }
 
   testingScope(true);
+
+    //Crie uma função que faça com que o array oddsAndEvens fique em ordem crescente;
+    //Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>);
+    // retorne a frase “Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!”.
+    //Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort().
+     //Spoiler: É possível realizar uma função que ordene qualquer array de números.
+    
+     const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+     const sortOddsAndEvens = () => {
+       oddsAndEvens[0] = 2;
+       oddsAndEvens[1] = 3;
+       oddsAndEvens[2] = 4;
+       oddsAndEvens[3] = 7; 
+       oddsAndEvens[4] = 10;
+       oddsAndEvens[5] = 13;
+       return oddsAndEvens;
+     };
+     const sortedArray = sortOddsAndEvens();
+     console.log(`Os números ${sortedArray} se encontram ordenados de forma crescente!`)
+     
